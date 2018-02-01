e053d3e4158824c606d302184c36efebdd48bddc
Cuong Nguyen <cnnguye1@yahoo.com>
2018-01-31 22:58:43 -0600

    added ASP_NetCore dir and Poke Assig

---
+++ ASP_NetCore/PokeInfo/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+namespace PokeInfo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /Home/
+        [HttpGet]
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        [Route("pokemon/{pokeid}")]
+        public IActionResult Pokemon(int pokeid)
+        {
+            var PokeInfo = new Dictionary<string, object>();
+            WebRequest.GetPokemonDataAsync(pokeid, ApiResponse =>
+                {
+                    PokeInfo = ApiResponse;
+                }
+            ).Wait();
+            // Other code
+            
+            ViewBag.Pok = PokeInfo;
+            return View();
+        }
+    }
+}
